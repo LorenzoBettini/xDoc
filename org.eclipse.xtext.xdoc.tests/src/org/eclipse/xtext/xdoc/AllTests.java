ff34cbb94c5b3713eec04160b69d842cc0d6664c
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2013-10-07 12:07:13 +0200

    test suite with working tests
    Change-Id: I2c44249adc83bcb058ee87571d130c5728a4f34c

---
+++ org.eclipse.xtext.xdoc.tests/src/org/eclipse/xtext/xdoc/AllTests.java
@@ -0,0 +1,13 @@
+package org.eclipse.xtext.xdoc;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	ParserTest.class,
+	UtilityTest.class
+})
+public class AllTests {
+
+}
